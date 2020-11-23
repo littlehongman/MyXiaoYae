533c548caa2b58bcbff20e416936227532afaf9f
00757001 <00757001@email.ntou.edu.tw>
2020-11-23 21:44:26 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	include("function/condb.php");
+?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>買宵夜</title>
+    <style></style>
+</head>
+</body>
+    <h1>買宵夜</h1>
+</body>
+</html>
