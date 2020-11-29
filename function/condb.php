526094acc08c33ab488d47fc4ec958ffdc519d90
00757001 <00757001@email.ntou.edu.tw>
2020-11-29 17:36:38 +0800

    Update condb.php

--- function/condb.php
+++ function/condb.php
@@ -1,6 +1,6 @@
 <?php
 	$user = 'root'; //資料庫使用者名稱
-	$password = ''; //資料庫的密碼
+	$password = 'root'; //資料庫的密碼
 	try{
 		$db = new PDO ('mysql: host=localhost;dbname=myxiaoyae; charset=utf8', $user, $password);
 		//之後若要結束與資料庫的連線，則使用「$db = null;」
@@ -11,11 +11,26 @@
 		die();
 	}
 	
-	//$userData = json_decode(file_get_contents("php://input"));
 	$data = array();
 	$received_data = json_decode(file_get_contents("php://input"));
+
 	if($received_data->action == 'fetchStore'){
 		$query = "SELECT * FROM store";
+		$statement = $db->prepare($query);
+		$statement->execute();
+		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+		{
+			$data[] = $row;
+		}
+		echo json_encode($data, JSON_UNESCAPED_UNICODE);
+	}	
+
+	if($received_data->action == 'fetchFood'){
+		$condition = "";
+		if(isset($_GET['store_name'])){
+			$condition = "store_name=".$_GET['store_name'];
+		}
+		$query = "SELECT * FROM food WHERE ".$condition;
 		$statement = $db->prepare($query);
 		$statement->execute();
 		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
