a8eddfa85a3207377099c36d0b133ce0fa85df42
00757001 <00757001@email.ntou.edu.tw>
2020-12-05 19:26:18 +0800

    Update order.php

--- order.php
+++ order.php
@@ -124,7 +124,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="btn-group btn-group-toggle mx-auto col-sm-7 " data-toggle="buttons">
                     <a href="index.php" class="btn btn-primary btn-lg">首頁</a>
-                    <a href="index.php" class="btn btn-primary btn-lg">編輯店家</a>
+                    <a href="store_edit.php" class="btn btn-primary btn-lg">編輯店家</a>
                     <a href="index.php" class="btn btn-primary btn-lg">編輯食物</a>
                 </div>
                 <form class="form-inline my-2 my-lg-0">
