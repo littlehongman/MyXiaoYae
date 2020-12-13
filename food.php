c4fcee4e908bfb3368e27b809c511d4c0d267030
00757001 <00757001@email.ntou.edu.tw>
2020-12-13 21:02:54 +0800

    Update food.php

--- food.php
+++ food.php
@@ -40,13 +40,6 @@
                                 console.log(response.data);
                             });
                         },
-                        // fetchStoreName:function(){
-                        //     axios.post('function/condb.php',{action:'fetchStoreName',id:this.storeName
-                        //     }).then(function(response){
-                        //         foodData.storeName = response.data;
-                        //         console.log(response.data);
-                        //     });
-                        // },
                         addOrder:function(foodID,index){
                             if(foodData.name[index] == '' || foodData.foodNumber[index] == null){
                                 if(foodData.name[index] == ''){
@@ -63,8 +56,8 @@
                                     fnumber:foodData.foodNumber[index]
                                 }).then(function(response){
                                     console.log(foodID);
-                                    foodData.name[index] = '';
-                                    foodData.foodNumber[index] = 0;
+                                    foodData.name[index] = null;
+                                    foodData.foodNumber[index] = null;
                                     alert(response.data);
                                 });
                             }
@@ -92,7 +85,7 @@
                 <div class="btn-group btn-group-toggle mx-auto col-sm-7 " data-toggle="buttons">
                     <a href="index.php" class="btn btn-primary btn-lg">首頁</a>
                     <a href="store_edit.php" class="btn btn-primary btn-lg">編輯店家</a>
-                    <a href="index.php" class="btn btn-primary btn-lg">編輯食物</a>
+                    <a href="food_edit.php" class="btn btn-primary btn-lg">編輯食物</a>
                 </div>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
