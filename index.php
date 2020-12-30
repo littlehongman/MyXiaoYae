1898be18e55962b41307cba9b6109aedcac8e267
changzz777 <abcjimmy35652251@gmail.com>
2020-12-31 00:23:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="btn-group btn-group-toggle mx-0 col-sm-7 " data-toggle="buttons">
                     <a href="index.php" class="btn btn-outline-primary btn-lg">首頁</a>
-                    <a href="store_edit.php" class="btn btn-outline-primary btn-lg">編輯店家</a>
+                    <a href="store_edit.php" class="btn btn-outline-warning btn-lg">編輯店家</a>
                     <a href="food_edit.php" class="btn btn-outline-primary btn-lg">編輯食物</a>
                 </div>
             </div>
